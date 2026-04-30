2282ab31e5f6fb8257081141ff2e48b44f3a4414
dkruse-rgb <dkruse1027@gmail.com>
2026-04-30 08:02:28 -0500

    Use cleaner live day animation app

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./AppV9.jsx";
+import App from "./AppV10.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
