82e992ee3a2f727a2d863e9284c80db7db05f97d
dkruse-rgb <dkruse1027@gmail.com>
2026-04-30 07:25:49 -0500

    Use weekly trends loyalty app

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./AppV3.jsx";
+import App from "./AppV4.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
