9ca8fbae019a55207b28798c6cdfd867336e760a
dkruse-rgb <dkruse1027@gmail.com>
2026-04-30 09:13:22 -0500

    Use clean live overlay app

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./AppV22.jsx";
+import App from "./AppV23.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
